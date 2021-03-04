6f8bfd914f38a625dfdfe31f0e5bff2e301a4226
Andrew Perez <andrewperez@Andrews-MacBook-Air.local>
2021-03-03 18:32:45 -0800

    removing keys.

--- keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {    
-    mongoUri:'mongodb+srv://newschooldrew:Xenape87@cluster0.d7uh1.mongodb.net/scc?retryWrites=true&w=majority',
-    SENDGRID_API_KEY:'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'
-}
